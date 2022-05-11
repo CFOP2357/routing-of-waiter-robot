14c63ea24090068ec4bcb10f5671c50f3a4d38b6
cfop2357 <yoguel.salazar@gmail.com>
2022-05-11 13:40:30 -0500

    move_forward

--- main.py
+++ main.py
@@ -8,13 +8,48 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
+# Initialize the motors.
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.D)
+
+# Initialize the color sensor.
+left_line_sensor = ColorSensor(Port.S3)
+right_line_sensor = ColorSensor(Port.S4)
+
+# Initialize the drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
+DRIVE_SPEED = 120
+
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
+def move_forward(speed):
+    """
+    Robot moves forward following a line to the next corner on the front
+    """
+    robot.drive(50, 0)
+    wait(1000)
+    while True:
+        left_is_white = left_line_sensor.reflection() > 15
+        right_is_white = right_line_sensor.reflection() > 15
+
+        if left_is_white and right_is_white:
+            robot.drive(speed, 0)
+        if left_is_white and not right_is_white:
+            robot.drive(speed, 30)
+        if not left_is_white and right_is_white:
+            robot.drive(speed, -30)
+        if not left_is_white and not right_is_white:
+            robot.stop(Stop.BRAKE)
+            break
+
+        wait(10)
 
 # Create your objects here.
 ev3 = EV3Brick()
 
-
 # Write your program here.
 ev3.speaker.beep()
+
+move_forward(DRIVE_SPEED)
